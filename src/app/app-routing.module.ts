f496af7a7c189bb04fd2e1511ab54836ea646683
Giovanna Melo <gimelosilva@outlook.com>
2022-07-12 11:05:44 -0300

    Primeira parte da NavBar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 const routes: Routes = [
-//ajustar para tela inicial
-  {path:'', redirectTo: 'login',pathMatch:'full'},
-  
+
+  {path:'', redirectTo: 'home',pathMatch:'full'},
   {path:'login', component:LoginComponent},
   {path:'cadastro', component:CadastroComponent},
-  {path: 'home', component:HomeComponent}
+  {path: 'home', component:HomeComponent},
+  {path: 'menu', component: MenuComponent},
+  {path: 'rodape', component: RodapeComponent}
 ];
 
 @NgModule({
