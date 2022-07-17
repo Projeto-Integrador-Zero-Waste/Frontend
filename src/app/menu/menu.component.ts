b7576524ef5ec1bb4135094ee961765cd9643781
Renan <baratellarenan@gmail.com>
2022-07-17 18:10:15 -0300

    Ajustado foto padrão para usuário caso não tenha

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -18,6 +18,15 @@ export class MenuComponent implements OnInit {
   ) { }
 
   ngOnInit(){
+    window.scroll(0,0)
+    this.validarFoto()
+  }
+
+  validarFoto() {
+
+    if (environment.foto == '') {
+      this.foto = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__480.png'
+    }
   }
 
   sair(){
